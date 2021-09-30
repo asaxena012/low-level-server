be4ef8674e1244c21a8a5c3fa0d901d3c7fd4dd3
asaxena012 <asaxena1205@gmail.com>
2021-09-30 14:51:20 +0530

    creating a server using express

---
+++ server.mjs
@@ -0,0 +1,33 @@
+import express from 'express';
+import morgan from 'morgan';
+import bp from 'body-parser';
+
+const app = express();
+
+// Parse POST body to JSON
+app.use(bp.json());
+
+// Logging middleware
+app.use(morgan('dev'))
+
+const db = []
+
+// Setting up routes and methods
+app.post('/todo', (req, res) => {
+    const newTodo = {
+        id: Date.now(),
+        text: req.body.text
+    }
+
+    db.push(newTodo)
+
+    res.json(newTodo)
+})
+
+app.get('/todo', (req, res) => {
+    res.json(db)
+})
+
+app.listen(8000, () => {
+    console.log(`Server on https://localhost:8000`)
+})
